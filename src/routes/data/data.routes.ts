cc469fe889c4e3331c19e3a7b1bcb8733a8ed922
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-10 12:26:22 -0300

    feat: add middleware to make the authentication

--- src/routes/data/data.routes.ts
+++ src/routes/data/data.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import Data from "../../controller/data";
+import authMiddleware from "../../middleware/auth.middleware";
 
 const dataRoutes = Router();
 
-dataRoutes.post('/', Data.getAll);
-dataRoutes.post('/history', Data.getHistory);
-dataRoutes.post('/schedule', Data.getSchedule);
+dataRoutes.post('/', authMiddleware, Data.getAll, );
+dataRoutes.post('/history', authMiddleware, Data.getHistory);
+dataRoutes.post('/schedule', authMiddleware, Data.getSchedule);
 
 export default dataRoutes;
