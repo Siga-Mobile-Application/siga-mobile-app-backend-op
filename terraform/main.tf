2b5499e12c79dba167c1be04958d853586dca538
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-16 20:54:26 -0300

    ci: fix dist files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,16 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_object" "dist" {
+  for_each = fileset(".../s3/", "**/*")
+
+  bucket = aws_s3_bucket.bucket.id
+  key    = each.value
+  source = ".../s3/${each.value}"
+  etag   = filemd5(".../s3/${each.value}")
+  content_type = "application/json"
+}
+
 data "aws_iam_role" "lambda-role" {
   name = "siga-app-lambda-role"
 }
