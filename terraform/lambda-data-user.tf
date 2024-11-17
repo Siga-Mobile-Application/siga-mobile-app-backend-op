e781b267611f93441b12d8394fe0c372880e39ce
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-16 22:24:42 -0300

    ci: fix reference to s3 object

--- terraform/lambda-data-user.tf
+++ terraform/lambda-data-user.tf
@@ -12,7 +12,7 @@ resource "aws_s3_object" "get_data_user" {
 resource "aws_lambda_function" "get_data_user" {
   source_code_hash = filebase64sha256(local.source_get_data_user)
   s3_bucket        = aws_s3_bucket.bucket.id
-  s3_key           = aws_s3_object.lambda_user.key
+  s3_key           = aws_s3_object.get_data_user.key
   function_name    = "${local.app_name}-${var.environment}-get-data"
   role             = data.aws_iam_role.lambda-role.arn
   handler          = "index.handler"
