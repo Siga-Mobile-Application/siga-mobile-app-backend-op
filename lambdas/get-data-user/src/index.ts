9dfbc6e10f52bd4e14198fa277b6b33df722ba3a
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-17 09:46:08 -0300

    ci: change lambda return

--- lambdas/get-data-user/src/index.ts
+++ lambdas/get-data-user/src/index.ts
@@ -74,7 +74,7 @@ async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyRe
 
         console.log('Going to home page...');
 
-        const result = await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 15000 }).then(() => {
+        const result = await page.waitForNavigation({ waitUntil: 'networkidle2' }).then(() => {
             return '';
         }).catch(async () => {
             const resultId = 'span_vSAIDA';
