30e01d1714d93fef9377a5527fc58c63dd95c9b3
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-17 07:17:59 -0300

    ci: set up apigateway

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -33,7 +33,7 @@ resource "aws_apigatewayv2_integration" "get_data_user" {
 
   integration_uri    = aws_lambda_function.get_data_user.invoke_arn
   integration_type   = "AWS_PROXY"
-  integration_method = "GET"
+  integration_method = "POST"
 }
 
 resource "aws_apigatewayv2_route" "get_data_user" {
