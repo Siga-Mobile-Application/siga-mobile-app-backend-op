bcde437783e9d84b0e0ac2b0c8ab7766c93ffd2b
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-16 21:23:24 -0300

    ci: fix dist files

--- terraform/lambda-data-user.tf
+++ terraform/lambda-data-user.tf
@@ -10,7 +10,7 @@ resource "aws_s3_object" "lambda_user" {
 }
 
 resource "aws_lambda_function" "hello_word" {
-  source_code_hash = filebase64sha256(local.source_get_data_user)
+  source_code_hash = filebase64sha256("../build/lambdas/hello.zip")
   s3_bucket = aws_s3_bucket.bucket.id
   s3_key = aws_s3_object.lambda_user.key
   function_name = "${local.app_name}-${var.environment}-hello-word"
