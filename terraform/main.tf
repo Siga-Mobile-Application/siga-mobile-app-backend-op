7c88a982837dfbd6d2eda42d7721f6c89b7e3049
carlosfiori44 <carlos.fiori@hotmail.com>
2024-11-16 21:09:47 -0300

    ci: fix dist files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,15 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+  for_each = true
 }
 
 resource "aws_s3_object" "dist" {
-  for_each = fileset(".../s3/", "**/*")
+  for_each = fileset("../s3/", "**/*")
 
   bucket = aws_s3_bucket.bucket.id
   key    = each.value
-  source = ".../s3/${each.value}"
-  etag   = filemd5(".../s3/${each.value}")
+  source = "../s3/${each.value}"
+  etag   = filemd5("../s3/${each.value}")
   content_type = "application/json"
 }
 
